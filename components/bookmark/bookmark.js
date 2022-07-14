6bc8ed3dd5030df8ce8f000126a5cc36e722c5a8
Kolja Woide <kolja.woide@gmail.com>
2022-07-14 15:23:17 +0200

    add linting

--- components/bookmark/bookmark.js
+++ components/bookmark/bookmark.js
@@ -1,15 +1,15 @@
 export default function Bookmark() {
-  const listOfBookmarks = document.querySelectorAll(".quiz-card__bookmark");
-  const listOfButtons = document.querySelectorAll(".quiz-card__button");
-  const listOfCards = document.querySelectorAll(".quiz-card");
+  const listOfBookmarks = document.querySelectorAll('.quiz-card__bookmark');
+  const listOfButtons = document.querySelectorAll('.quiz-card__button');
+  const listOfCards = document.querySelectorAll('.quiz-card');
 
   for (let i = 0; i < listOfBookmarks.length; i++) {
     const bookmark = listOfBookmarks[i];
-    bookmark.addEventListener("click", () => {
-      if (bookmark.textContent === "bookmark") {
-        bookmark.textContent = "bookmark_border";
+    bookmark.addEventListener('click', () => {
+      if (bookmark.textContent === 'bookmark') {
+        bookmark.textContent = 'bookmark_border';
       } else {
-        bookmark.textContent = "bookmark";
+        bookmark.textContent = 'bookmark';
       }
     });
   }
