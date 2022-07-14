a548452ba9ecb819a35a67261f2b6f2e69a21341
Kolja Woide <kolja.woide@gmail.com>
2022-07-14 14:11:55 +0200

    update all

---
+++ components/button/button.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < listOfButtons.length; i++) {
+  const button = listOfButtons[i];
+  const card = listOfCards[i];
+  button.addEventListener("click", () => {
+    if (button.textContent === "Show Answer") {
+      button.textContent = "Hide Answer";
+    } else {
+      button.textContent = "Show Answer";
+    }
+    button.classList.toggle("quiz-card__button--toggled");
+    card.classList.toggle("quiz-card--opened");
+  });
+}
